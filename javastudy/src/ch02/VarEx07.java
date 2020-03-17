623f7a122e12d7a6b65d7fd8fb20514bc746dfcb
jaybon1 <ponggas2@gmail.com>
2020-03-17 16:00:35 +0900

    1600

---
+++ javastudy/src/ch02/VarEx07.java
@@ -0,0 +1,16 @@
+package ch02;
+
+public class VarEx07 {
+	public static void main(String[] args) {
+		// 타입을 Object로 만들면 추론하기 힘들어짐
+		// 받을 것이 모호할때만 제한적으로 사용 
+		Object n1 = 1;
+		Object n2 = '가';
+		Object n3 = "문자열";
+		Object n4 = 10.5;
+		System.out.println(n1);
+		System.out.println(n2);
+		System.out.println(n3);
+		System.out.println(n4);
+	}
+}
